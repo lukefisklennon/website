99daa78f0f1bd9237cf63d7430de8001b6a9483f
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2020-05-15 01:30:04 +1000

    Performance and interactivity

--- src/index.js
+++ src/index.js
@@ -3,44 +3,69 @@ import "./style.css"
 const element = document.getElementById("canvas")
 let canvas = element.getContext("2d")
 
-const radiusRange = [100, 150]
-// const velocityRange = [0.2, 0.8]
-const velocityRange = [0.04, 0.16]
-const totalCircles = 150
+const blur = 8
+const shrink = 20
+const fps = 30
+const totalCircles = 50
+
+const radiusRange = [150 / shrink, 250 / shrink]
+const velocityRange = [15 / shrink / fps, 30 / shrink / fps]
+const hueRange = [15, 345]
+const hueVelocityRange = [4 / fps, 8 / fps]
+
+element.style.transform = `translate(-50%, -50%) scale(${shrink + 2})`
+element.style.filter = `blur(${blur}px) contrast(0.5) saturate(0.7) brightness(1.8)`
+
+const mouseDeltaFactor = 0.2
+let mouseLast = null
+let mouseStopTimer = null
+let mouseDelta = 0
+
+window.onmousemove = event => {
+	if (mouseLast) {
+		mouseDelta = Math.sqrt((event.clientX - mouseLast[0]) ** 2 + (event.clientY - mouseLast[1]) ** 2)
+	} else {
+		mouseDelta = 0
+	}
+	mouseLast = [event.clientX, event.clientY]
+	clearTimeout(mouseStopTimer)
+	mouseStopTimer = setTimeout(() => {
+		mouseDelta = 0
+	}, 50)
+}
 
 let circles = []
 
 class Circle {
 	constructor() {
-		this.position = [random(innerWidth), random(innerHeight)]
+		this.position = [random(innerWidth / shrink), random(innerHeight / shrink)]
 		this.velocity = [random(...velocityRange, true), random(...velocityRange, true)]
 		this.radius = random(...radiusRange)
-		this.fill = `hsl(${random(15, 345)}, 100%, 50%)`
-		this.hue = random(15, 345)
-		this.hueVelocity = random(0.08, 0.16, true)
+		this.hue = random(...hueRange)
+		this.hueVelocity = random(...hueVelocityRange, true)
 	}
 
-	render() {
-		this.position[0] += this.velocity[0]
-		this.position[1] += this.velocity[1]
+	render(delta) {
+		this.position[0] += this.velocity[0] * delta
+		this.position[1] += this.velocity[1] * delta
 
-		if (this.position[0] > innerWidth + this.radius) {
+		if (this.position[0] > innerWidth / shrink + this.radius) {
 			this.position[0] = 0 - this.radius
 		}
 
 		if (this.position[0] < 0 - this.radius) {
-			this.position[0] = innerWidth + this.radius
+			this.position[0] = innerWidth / shrink + this.radius
 		}
 
-		if (this.position[1] > innerHeight + this.radius) {
+		if (this.position[1] > innerHeight / shrink + this.radius) {
 			this.position[1] = 0 - this.radius
 		}
 
 		if (this.position[1] < 0 - this.radius) {
-			this.position[1] = innerHeight + this.radius
+			this.position[1] = innerHeight / shrink + this.radius
 		}
 
-		this.hue += this.hueVelocity
+		this.hue += this.hueVelocity * delta
 		if (this.hue > 360) this.hue = 0
 		if (this.hue < 0) this.hue = 360
 
@@ -56,13 +81,13 @@ for (let i = 0; i < totalCircles; i++) {
 	circles.push(new Circle())
 }
 
-const render = () => {
+setInterval(() => {
+	// todo: scale stuff over time
+	canvas.globalCompositeOperation = "source-over" // xor, hard-light, luminosity
 	canvas.clearRect(0, 0, innerWidth, innerHeight)
 	canvas.globalCompositeOperation = "hard-light" // xor, hard-light, luminosity
-	circles.forEach(circle => circle.render())
-	requestAnimationFrame(render)
-}
-render()
+	circles.forEach(circle => circle.render(1 + mouseDelta * mouseDeltaFactor))
+}, 1000 / fps)
 
 function random(a, b, mirror) {
 	if (b === undefined) {
@@ -78,10 +103,10 @@ function random(a, b, mirror) {
 
 function resize() {
 	const dpr = devicePixelRatio || 1
-	element.style.width = `${innerWidth}px`
-	element.style.height = `${innerHeight}px`
-	element.width = innerWidth * dpr
-	element.height = innerHeight * dpr
+	element.style.width = `${innerWidth / shrink}px`
+	element.style.height = `${innerHeight / shrink}px`
+	element.width = innerWidth / shrink * dpr
+	element.height = innerHeight / shrink * dpr
 	canvas.scale(dpr, dpr)
 }
 
